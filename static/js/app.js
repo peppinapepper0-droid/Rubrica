2912589a46df386ced9cf2cd6f1d782c4eb4ee39
Arianna Tagliavini <arianna.tagliavini@fermi.mo.it>
2025-12-11 12:54:41 +0100

    funzioni login e logout nel .js

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,29 @@ async function login() {
         location.href = "tasks.html";
 }
 
+async function registerUser() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch("/api/register", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
+    });
+
+    const data = await res.json();
+    alert(data.message || data.error);
+
+    if (res.status === 201)
+        location.href = "login.html";
+}
+
+
+async function logout() {
+    await fetch("/api/logout", {method: "POST"});
+    location.href = "login.html";
+}
+
 //MODIFICO FUNZIONE PER CARICARE I MSG SUL SERVER
 async function loadTasks() {
 
